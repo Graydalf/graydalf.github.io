b30adb3b6c05f9cbd62d70714165b0dff4172c10
graydalf <graydalf@gmail.com>
2017-09-29 18:18:35 +0800

    docs: any

---
+++ gulp-custom-plug/gulp-minify-space.js
@@ -0,0 +1,53 @@
+// through2 是一个对 node 的 transform streams 简单封装
+var through = require('through2')
+var gutil = require('gulp-util')
+var PluginError = gutil.PluginError
+
+// 常量
+const PLUGIN_NAME = 'gulp-prefixer'
+
+function prefixStream(prefixText) {
+  var stream = through()
+  stream.write(prefixText)
+  return stream
+}
+
+function filter(value) {
+  return function(val) {
+    return val != value
+  }
+}
+
+// 插件级别函数 (处理文件)
+function gulpPrefixer() {
+
+  var jsReg = ''
+  var cssReg = ''
+  var htmlReg = />\s*([^<>\f\n\r\t\v​\u00a0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​​\u202f\u205f​\u3000]*)\s*</g
+
+  return through.obj(function(chunk, enc, callback) {
+
+    if (chunk.isNull()) {
+      // 返回空文件
+      callback(null, chunk)
+    }
+
+    console.log(chunk.history[0])
+
+    if (chunk.history[0].endsWith('.html')) {
+      var str = chunk.contents.toString().trim()
+      str = str.replace(htmlReg, '>' + '$1'.trim() + '<')
+      chunk.contents = Buffer.from(str)
+    } else if (chunk.history[0].endsWith('.html')) {
+
+    } else if (chunk.history[0].endsWith('.html')) {
+
+    }
+
+    callback(null, chunk)
+  })
+
+}
+
+// 暴露（export）插件主函数
+module.exports = gulpPrefixer
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var del = require('del');
+var minifySpace = require('./gulp-custom-plug/gulp-minify-space');
+
+// gulp.task('default', function() {
+//     gulp.src('src/*.html', { base: 'src' })
+//         .pipe(minify({
+//             ext: {
+//                 src: '.html',
+//                 min: '.html'
+//             },
+//             exclude: ['tasks'],
+//             ignoreFiles: ['.combo.js', '-min.js']
+//         }))
+//         .pipe(gulp.dest('build'))
+// })
+
+gulp.task('default', function() {
+  gulp.src('src/*.*', { base: 'src' })
+    .pipe(minifySpace())
+    .pipe(gulp.dest('build'));
+
+  gulp.src('src/css/*.*', { base: 'src' })
+    .pipe(minifySpace())
+    .pipe(gulp.dest('build'));
+
+  gulp.src('src/js/*.*', { base: 'src' })
+    .pipe(minifySpace())
+    .pipe(gulp.dest('build'));
+
+  gulp.src('src/img/*.*', { base: 'src' })
+    .pipe(minifySpace())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function(cb) {
+  del(['build/'], cb)
+});
