9075d282126d7cf3dcb8b727a1c8da137d840f22
graydalf <graydalf@gmail.com>
2017-09-29 19:36:57 +0800

    docs(update): fix gulp-minify-space regexp

--- gulp-custom-plug/gulp-minify-space.js
+++ gulp-custom-plug/gulp-minify-space.js
@@ -24,7 +24,7 @@ function gulpPrefixer() {
   var jsReg = '',
     cssReg = '',
     htmlReg = />\s*([^<>]*)</g,
-    extSpace = /[^\f\n\r\t\v​\u00a0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​​\u202f\u205f​\u3000]*/g,
+    extSpace = /[\f\n\r\t\v​\u00a0\u1680​\u180e\u2000​\u2001\u2002​\u2003\u2004​\u2005\u2006​\u2007\u2008​\u2009\u200a​\u2028\u2029​​\u202f\u205f​\u3000]*/g,
     htmlRegExp = /<!--[^\n]*\n/g
 
   return through.obj(function(chunk, enc, callback) {
