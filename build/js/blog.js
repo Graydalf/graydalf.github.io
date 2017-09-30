5efc612bf213f51a78dc7fcb7e60dce513a7d4f5
graydalf <graydalf@gmail.com>
2017-10-01 03:55:07 +0800

    docs(api): any

--- build/js/blog.js
+++ build/js/blog.js
@@ -1,75 +1,147 @@
-(function() {
+(function () {
 
-  var tmpDOM, i, j, usr, get
-
-  get = function(url, callback) {
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-      //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-      xmlhttp = new XMLHttpRequest()
-    } else {
-      // IE6, IE5 浏览器执行代码
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
-    }
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        callback(null, xmlhttp.responseText)
-      } else {
-        callback('err', null)
-      }
-    }
-    xmlhttp.open('GET', url, true)
-    xmlhttp.send()
-  }
+  var tmpDOM, i, usr, init, buildTree;
+  var LOCALSTORAGE_USR_KEY = 'usr';
 
   /**
    * usr
    */
-  // usr = localStorage.getItem('note-usr')
-  // if (!usr) {
-  //   window.location.href = './login.html'
-  // }
+  usr = localStorage.getItem(LOCALSTORAGE_USR_KEY);
 
-  // get(`http://www.graydalf.com/githubio/note/?usr=${usr.name}&token=${usr.token}`, function(err, data) {
-  //   if (err) {
-  //     localStorage.removeItem('note-usr')
-  //     window.location.href = './login.html'
-  //     return
-  //   }
-
-  //   //TODO data
-  //   console.log(data);
-  // })
-
-  /**
-   * aside doc-tree hide or show
-   */
-  document.querySelector('main>aside>.doc-tree-taggle').addEventListener('click', function() {
-    var asideDOM = document.querySelector('main>aside')
-    if (asideDOM.className.includes('drawer')) {
-      asideDOM.className = asideDOM.className.replace(/drawer/, '').trim()
-    } else {
-      asideDOM.className += ' drawer'
-    }
-  })
-
-  /**
-   * aside doc-tree click event
-   */
-  tmpDOM = document.querySelectorAll('main>aside>.doc-tree span')
-  for (i = 0; i < tmpDOM.length; i++) {
-    tmpDOM[i].addEventListener('click', function(item) {
-      var ulDOM = item.target.nextSibling
-      if (ulDOM.hasAttribute('hidden')) {
-        ulDOM.removeAttribute('hidden')
-      } else {
-        ulDOM.setAttribute('hidden', 'hidden')
-      }
-    })
+  if (!usr) {
+    window.location.href = './login.html'
   }
 
+  usr = JSON.parse(usr);
 
+  $.ajax({
+    url: '/api/notes/getTree',
+    data: {
+      usr: usr.name,
+      token: usr.token
+    },
+    success: function (res) {
+      console.log(res);
+      if (res.code === 0) {
+        init(res.data);
+      } else {
+        window.location.href = './login.html'
+      }
+    },
+    dataType: 'json'
+  });
 
+  document.querySelector('#usr').innerHTML = usr.name;
 
+  document.querySelector('#out').addEventListener('click', function () {
+    localStorage.removeItem(LOCALSTORAGE_USR_KEY);
+    window.location.href = './login.html'
+  });
 
-}())
+  /**
+   * 
+   */
+  document.querySelector('#update').addEventListener('click', function () {
+    $.ajax({
+      url: '/api/notes/update',
+      data: {
+        usr: usr.name
+      },
+      dataType: 'json',
+      success: function (res) {
+        console.log(res);
+        if (res.code === 0) {
+          //
+        } else {
+          var gitpath = prompt("git registory:", res.msg);
+          $.ajax({
+            url: '/api/notes/update',
+            data: {
+              usr: usr.name,
+              gitpath: gitpath
+            },
+            dataType: 'json',
+            success: function (res) {
+              console.log(res);
+              if (res.code === 0) {
+                //
+              }
+            }
+          });
+        }
+      }
+    });
+  });
+
+  init = function (data) {
+
+    $('.doc-tree').html('');
+    $('.doc-tree').append($("<h2>Document tree</h2>"));
+    buildTree(data, $('.doc-tree'));
+
+    /**
+     * aside doc-tree hide or show
+     */
+    document.querySelector('main>aside>.doc-tree-taggle').addEventListener('click', function () {
+      var asideDOM = document.querySelector('main>aside');
+      if (asideDOM.className.includes('drawer')) {
+        asideDOM.className = asideDOM.className.replace(/drawer/, '').trim();
+      } else {
+        asideDOM.className += ' drawer';
+      }
+    });
+
+    /**
+     * aside doc-tree click event
+     */
+    tmpDOM = document.querySelectorAll('main>aside>.doc-tree span');
+    for (i = 0; i < tmpDOM.length; i++) {
+      tmpDOM[i].addEventListener('click', function (item) {
+        var ulDOM = item.target.nextSibling;
+        if (ulDOM.hasAttribute('hidden')) {
+          ulDOM.removeAttribute('hidden');
+        } else {
+          ulDOM.setAttribute('hidden', 'hidden');
+        }
+      });
+    }
+  };
+
+  buildTree = function (tree, $doc) {
+
+    let $ul = $("<ul></ul>");
+
+    $doc.append($ul);
+
+    for (let key in tree.node) {
+      let node = tree.node[key];
+      let $li = $("<li></li>");
+      $ul.append($li);
+      if (node.type == 'dir') {
+        let $leaf = $(`<span>${node.name}</span>`);
+        $li.append($leaf);
+        buildTree(node, $li);
+      } else if (node.type == 'file') {
+        let path = node.node.replace(/\\/g, '\\\\');
+        let $leaf = $(`<a href="#" onclick="load('${path}')"> ${node.name}</a>`);
+        $li.append($leaf);
+      }
+    }
+  }
+}());
+
+function load(arg) {
+  $.ajax({
+    url: "/api/notes/note",
+    dataType: 'html',
+    data: {
+      path: arg
+    },
+    success: function (result) {
+      $('main>article>section').html(result);
+    },
+    error: function (err) {
+      console.log(JSON.stringify(err));
+    }
+  });
+}
--- build/js/login.js
+++ build/js/login.js
@@ -1,46 +1,37 @@
-(function() {
+(function () {
 
-  var get
-
-  get = function(url, callback) {
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-      //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-      xmlhttp = new XMLHttpRequest()
-    } else {
-      // IE6, IE5 浏览器执行代码
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
-    }
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        callback(null, xmlhttp.responseText)
-      } else {
-        callback('err', null)
-      }
-    }
-    xmlhttp.open('GET', url, true)
-    xmlhttp.send()
-  }
+  var LOCALSTORAGE_USR_KEY = 'usr';
 
   /**
    * aside doc-tree hide or show
    */
-  document.querySelector('main>article>input:nth-child(3)').addEventListener('click', function() {
-    var usr = document.querySelector('main>article>input:nth-child(1)').nodeValue;
-    var pwd = document.querySelector('main>article>input:nth-child(2)').nodeValue;
-    if (usr && pwd) {
-      get(`http://www.graydalf.com/githubio/login/?usr=${usr}&pwd=${pwd}`, function(err, data) {
-        if (err) {
-          localStorage.removeItem('note-usr')
-          window.location.href = '../login.html'
-          alert('login err')
-          return
-        }
+  document.querySelector('main>article>input:nth-child(3)').addEventListener('click', function () {
+    var usr = document.querySelector('main>article>input:nth-child(1)').value;
+    var pwd = document.querySelector('main>article>input:nth-child(2)').value;
 
-        //TODO data
-        console.log(data)
-        localStorage.setItem('note-usr', data)
-      })
+    console.log(usr+', '+pwd);
+    if (usr && pwd) {
+      $.ajax({
+        url: '/api/users/login',
+        data: {
+          usr: usr,
+          pwd: pwd
+        },
+        success: function (res) {
+          console.log(res);
+          if (res.code === 0) {
+            localStorage.setItem(LOCALSTORAGE_USR_KEY, JSON.stringify({
+              name: usr,
+              token: res.data.token
+            }));
+            window.location.href = './blog.html';
+          } else {
+            localStorage.removeItem(LOCALSTORAGE_USR_KEY);
+            alert(res.msg);
+          }
+        },
+        dataType: 'json'
+      });
     }
   })
 
